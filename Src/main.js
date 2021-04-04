01129a7cf4560e87c2144bcd97df1e0812c042b5
hurryingauto3 <ah05084@st.habib.edu.pk>
2021-04-05 01:14:32 +0500

    reorganized code

--- Src/main.js
+++ Src/main.js
@@ -15,7 +15,7 @@ app.on('ready', function(){
   mainWindow = new BrowserWindow({});
   
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'pages/index.html'),
     protocol: 'file:',
     slashes: true
   }));
