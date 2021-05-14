a8a9e58e9a1afc9ceaed65b1c615bd78811bfaeb
hurryingauto3 <ah05084@st.habib.edu.pk>
2021-05-15 02:09:05 +0500

    temp fix

--- Src/main.js
+++ Src/main.js
@@ -8,20 +8,20 @@ const glob = require('glob')
 const {app, BrowserWindow, dialog} = electron;
 
 const Database = require(path.resolve('./databaseInit.js'));
-const mainMenuTemplate = require('./menu')
+// const mainMenuTemplate = require('./menu')
 let MaarufDB = new Database('MaarufDB');
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 600,  
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true, 
       contextIsolation: false, 
       enableRemoteModule: true, 
-      nodeIntegrationInWorker: true
+      // nodeIntegration InWorker: true
     }
 
   
@@ -29,7 +29,7 @@ function createWindow () {
 
   MaarufDB.addBook(new Date(), "Hello");
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile('pdf.html');
 
   //build menu from template
   const mainMenu = Menu.buildFromTemplate((mainMenuTemplate));
@@ -62,3 +62,155 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+//Handle Add Book
+function getFileFromUser() {
+  var file = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+  var filestring
+  file.then(data => {
+    // console.log(data);
+    filestring = data.filePaths;
+    // console.log(filestring);
+    MaarufDB.addBook(new Date(), filestring);
+    console.log("book added")
+  })
+  // return filestring; //Not working for return 
+  
+};
+
+//Handle Add Directory
+function getDirFromUser (){
+  var file = dialog.showOpenDialog({
+    properties: ['openDirectory'],    
+  });
+  
+  file.then(data => {
+    // console.log(data);
+    // console.log(filestring);
+    searchRecursive(data.filePaths[0], '.pdf');
+  })
+
+  
+
+};
+
+//Recusively search folders for PDFs
+function searchRecursive(dir, pattern) {
+  // This is where we store pattern matches of all files inside the directory
+  var results = [];
+
+  // Read contents of directory
+  fs.readdirSync(dir).forEach(function (dirInner) {
+    // Obtain absolute path
+    dirInner = path.resolve(dir, dirInner);
+
+    // Get stats to determine if path is a directory or a file
+    var stat = fs.statSync(dirInner);
+
+    // If path is a directory, scan it and combine results
+    if (stat.isDirectory()) {
+      results = results.concat(searchRecursive(dirInner, pattern));
+    }
+
+    // If path is a file and ends with pattern then push it onto results
+    if (stat.isFile() && dirInner.endsWith(pattern)) {
+      results.push(dirInner);
+
+    }
+    
+  });
+  
+  var myJsonString = JSON.stringify(results);
+  writeJSON(myJsonString);
+  return results;
+};
+
+//Writes cotents of any JS object to JSON
+function writeJSON(jsonString){
+  fs.writeFile('./books.json', jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+    } else {
+        console.log('Successfully wrote file')
+    }
+})
+};
+//Menu Items
+const mainMenuTemplate = [
+  {
+    label: 'File', 
+    submenu: [
+      {
+        label: 'Add Book',
+        accelerator: 'Ctrl+O',
+        click(){
+          getFileFromUser();
+        }
+      }, 
+      {
+        label: 'Add Directory',
+        accelerator: 'Ctrl+D',
+        click(){
+          directory = getDirFromUser();
+          // console.log(type(directory))
+          
+        }
+      },
+      {
+        label: 'New Collection'
+      },
+      {
+        label: 'Open Book in Reader',
+        click(){
+          openPDF();
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  },
+
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Reload",
+        accelerator: "F5",
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            // on reload, start fresh and close any old
+            // open secondary windows
+            if (focusedWindow.id === 1) {
+              BrowserWindow.getAllWindows().forEach(win => {
+                if (win.id > 1) win.close();
+              });
+            }
+            focusedWindow.reload();
+          }
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Alt+Command+I'
+          } else {
+            return 'Ctrl+Shift+I'
+          }
+        })(),
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.toggleDevTools()
+          }
+        }
+      }
+    ]
+  }
+]
+
